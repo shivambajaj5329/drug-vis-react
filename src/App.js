4186cbfd05f7bf60fc1fc4aa59fc3394e4749e57
Shivam Bajaj <shivam.bajaj@batonsystems.com>
2021-12-02 01:36:27 -0500

    Almost fixed Map

--- src/App.js
+++ src/App.js
@@ -1,29 +1,87 @@
 //import {MapContainer,TileLayer,Marker ,Popup } from "react-leaflet"
+import React, {Component} from "react";
+import "leaflet/dist/leaflet.css"
+import { MapContainer , GeoJSON } from "react-leaflet"
+import "./styles/MyMap.css"
 import './App.css';
-import MyMap from "./components/MyMap.jsx";
-import BarChart from './components/BarChart'
+//import MyMap from "./components/MyMap.jsx";
+//import React, {Component} from 'react';
+import cocaine_data from './data/data_cocaine.json'
+import illicit_drugs from './data/data_scaled_correctly.json'
 
 
 
-function App() {
-  //const position = [51.505, -0.09]
-  //return //(
-  // <MapContainer center={position} zoom={13} scrollWheelZoom={false} style={{ height: 80, width: 80 }}>
-  //   <TileLayer
-  //     attribution='&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
-  //     url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
-  //   />
-  //   <Marker position={position}>
-  //     <Popup>
-  //       A pretty CSS3 popup. <br /> Easily customizable.
-  //     </Popup>
-  //   </Marker>
-  // </MapContainer>
+// function App() {
+//   return <MyMap />;
+// // return <BarChart data={data} />
 
+// }
+// return <MyMap />;
 
- // );
-  return <MyMap />;
-// return <BarChart data={data} />
+class App extends Component {
+  
+
+state = {
+  data: illicit_drugs
+};
+constructor(props){
+  super(props)
+  this.onRadioSelect = this.onRadioSelect.bind(this)
 }
 
+onRadioSelect = (event) =>{
+  const radio_value = event.target.value
+  let data
+
+  if (radio_value === "illicit_drugs"){
+    data = illicit_drugs
+  }
+  else if (radio_value === "cocaine"){
+    data = cocaine_data
+  }
+  this.setState({
+    data
+  })
+}
+
+countryStyle = {
+  fillColor: "red",
+  color: "black",
+  fillOpacity: 0.3,
+};
+
+OnEachState = (state, layer) => {
+  const stateName = state.properties.NAME
+  const stateWiseDrugData = state.properties.avg_nrm_data
+  layer.bindPopup(stateName + stateWiseDrugData)
+  layer.on({
+      click: (event) => {
+          console.log("click")
+      }
+  })
+  layer.options.fillOpacity = stateWiseDrugData
+
+}
+
+render(){
+  const {data} = this.state;
+  console.log(data)
+  return(
+
+    <>
+    <label htmlFor='illicit_drugs'> illicit drugs   </label>
+      <input type = "radio" placeholder='illicit drugs' value="illicit_drugs" id = "illicit_drugs" onChange={this.onRadioSelect} name='drugs' />
+      <label htmlFor='cocaine'> cocaine   </label>
+      <input type = "radio" placeholder='cocaine' value="cocaine" id = "cocaine" onChange={this.onRadioSelect} name='drugs'/>
+      <div>
+                <h1 style = { {textAlign:'center'}}>Drug Use Visualisation</h1>
+                <MapContainer style ={{height:"80vh"}} zoom ={4} center={[37.6000, -95.6650]}>
+                <GeoJSON data = {data.features} style = {this.countryStyle} onEachFeature ={this.OnEachState}></GeoJSON>
+                </MapContainer>
+                {/* <BarChart data={datas} /> */}
+            </div>
+    </>
+  )
+}
+}
 export default App;
--- src/components/MyMap.jsx
+++ src/components/MyMap.jsx
@@ -1,59 +1,21 @@
 import React, {Component} from "react";
-import data from "../data/data_scaled_correctly.json"
 import "leaflet/dist/leaflet.css"
 import { MapContainer , GeoJSON } from "react-leaflet"
 import "../styles/MyMap.css"
-import ToggleButton from "react-bootstrap/ToggleButton";
-import ButtonGroup from "react-bootstrap/ButtonGroup";
-import "bootstrap/dist/css/bootstrap.min.css";
-import BarChart from "./BarChart"
+
+// import { MDBFormInline, MDBInput } from 'mdbreact';
+
 
   
 
-const datas = [
-    {year: 1980, efficiency: 24.3, sales: 8949000},
-    {year: 1985, efficiency: 27.6, sales: 10979000},
-    {year: 1990, efficiency: 28, sales: 9303000},
-    {year: 1991, efficiency: 28.4, sales: 8185000},
-    {year: 1992, efficiency: 27.9, sales: 8213000},
-    {year: 1993, efficiency: 28.4, sales: 8518000},
-    {year: 1994, efficiency: 28.3, sales: 8991000},
-    {year: 1995, efficiency: 28.6, sales: 8620000},
-    {year: 1996, efficiency: 28.5, sales: 8479000},
-    {year: 1997, efficiency: 28.7, sales: 8217000},
-    {year: 1998, efficiency: 28.8, sales: 8085000},
-    {year: 1999, efficiency: 28.3, sales: 8638000},
-    {year: 2000, efficiency: 28.5, sales: 8778000},
-    {year: 2001, efficiency: 28.8, sales: 8352000},
-    {year: 2002, efficiency: 29, sales: 8042000},
-    {year: 2003, efficiency: 29.5, sales: 7556000},
-    {year: 2004, efficiency: 29.5, sales: 7483000},
-    {year: 2005, efficiency: 30.3, sales: 7660000},
-    {year: 2006, efficiency: 30.1, sales: 7762000},
-    {year: 2007, efficiency: 31.2, sales: 7562000},
-    {year: 2008, efficiency: 31.5, sales: 6769000},
-    {year: 2009, efficiency: 32.9, sales: 5402000},
-    {year: 2010, efficiency: 33.9, sales: 5636000},
-    {year: 2011, efficiency: 33.1, sales: 6093000},
-    {year: 2012, efficiency: 35.3, sales: 7245000},
-    {year: 2013, efficiency: 36.4, sales: 7586000},
-    {year: 2014, efficiency: 36.5, sales: 7708000},
-    {year: 2015, efficiency: 37.3, sales: 7517000},
-    {year: 2016, efficiency: 37.7, sales: 6873000},
-    {year: 2017, efficiency: 39.4, sales: 6081000},
-  ]
-
 class MyMap extends Component{
-    
-    state = {}
-    componentDidMount(){
-        console.log(data)
+
+    state = {
+        data: this.props.data
     }
 
-    
-
     countryStyle = {
-        fillColor: "blue",
+        fillColor: "red",
         color: "black",
         fillOpacity: 0.3,
     };
@@ -61,7 +23,6 @@ class MyMap extends Component{
     OnEachState = (state, layer) => {
         const stateName = state.properties.NAME
         const stateWiseDrugData = state.properties.avg_nrm_data
-        console.log(stateName);
         layer.bindPopup(stateName + stateWiseDrugData)
         layer.on({
             click: (event) => {
@@ -71,20 +32,22 @@ class MyMap extends Component{
         layer.options.fillOpacity = stateWiseDrugData
 
     }
-
-
-
-
-
+    componentDidUpdate(prevprops, prevstate)  {
+        const {data} = prevprops
+        if (JSON.stringify(data) !== JSON.stringify(this.props.data)){
+            this.setState({data:this.props.data})
+        }
+    }
     render(){
+        const {data} = this.state
+        console.log(data)
         return (
             <div>
-                
                 <h1 style = { {textAlign:'center'}}>Drug Use Visualisation</h1>
                 <MapContainer style ={{height:"80vh"}} zoom ={4} center={[37.6000, -95.6650]}>
                 <GeoJSON data = {data.features} style = {this.countryStyle} onEachFeature ={this.OnEachState}></GeoJSON>
                 </MapContainer>
-                <BarChart data={datas} />
+                {/* <BarChart data={datas} /> */}
                 
             </div>
 
